25151c79c2b8aeec4ff0600f42688aad4d35abd9
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-10 00:02:41 -0300

    [feat] finishing adapters

--- src/main/java/com/saito/inventory/adapters/out/SendUpdateInventoryAdapter.java
+++ src/main/java/com/saito/inventory/adapters/out/SendUpdateInventoryAdapter.java
@@ -1,14 +1,22 @@
 package com.saito.inventory.adapters.out;
 
+import com.saito.inventory.adapters.out.message.SaleMessage;
 import com.saito.inventory.application.core.domain.Sale;
 import com.saito.inventory.application.core.domain.enums.SaleEvent;
 import com.saito.inventory.application.ports.out.SendUpdatedInventoryOutPutPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendUpdateInventoryAdapter implements SendUpdatedInventoryOutPutPort {
+
+    @Autowired
+    private KafkaTemplate<String, SaleMessage> kafkaTemplate;
+
     @Override
     public void send(Sale aSale, SaleEvent aSaleEvent) {
-
+        var saleMessage = new SaleMessage(aSale, aSaleEvent);
+        kafkaTemplate.send("tp-saga-sale", saleMessage);
     }
 }
